a4abc05474ff3d2de5fff017c9057f74dd8b2938
azesm <azesm@yahoo.com>
2021-04-18 14:14:54 +0300

    modal html-css

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,3 +1,5 @@
+(function ($) {
+
 const hamburger = document.querySelector('.hamburger'),
     menu = document.querySelector('.menu'),
     closeElem = document.querySelector('.menu__close');
@@ -16,3 +18,40 @@ const counters = document.querySelectorAll('.skills__ratings-counter'),
 counters.forEach((item, i) => {
     lines[i].style.width = item.innerHTML;
 });
+
+
+$('[data-modal=consultation]').on('click', function () {
+    $('.overlay, #consultation').fadeIn('slow');
+});
+$('.modal__close').on('click', function () {
+    $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+});
+
+$('.button_mini').each(function (i) {
+    $(this).on('click', function () {
+        $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+        $('.overlay, #order').fadeIn('slow');
+    });
+});
+
+
+
+})(jQuery);
+
+//$('form').submit(function (e) {
+//    e.preventDefault();
+//    $.ajax({
+//        type: "POST",
+//        url: "mailer/smart.php",
+//        data: $(this).serialize()
+//    }).done(function () {
+//        $(this).find("input").val("");
+//        $('#consultation, #order').fadeOut();
+//        $('.overlay, #thanks').fadeIn('slow');
+
+
+
+//        $('form').trigger('reset');
+//    });
+//    return false;
+//});
