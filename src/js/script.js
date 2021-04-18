df4ad0fa5460c65350c5f10c35a55216de83ac19
azesm <azesm@yahoo.com>
2021-04-18 14:41:47 +0300

    post php

--- src/js/script.js
+++ src/js/script.js
@@ -20,19 +20,25 @@ counters.forEach((item, i) => {
 });
 
 
-$('[data-modal=consultation]').on('click', function () {
-    $('.overlay, #consultation').fadeIn('slow');
-});
-$('.modal__close').on('click', function () {
-    $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
-});
 
-$('.button_mini').each(function (i) {
-    $(this).on('click', function () {
-        $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
-        $('.overlay, #order').fadeIn('slow');
-    });
-});
+
+
+
+//MODAL
+
+//$('[data-modal=consultation]').on('click', function () {
+//    $('.overlay, #thanks').fadeIn('slow');
+//});
+//$('.modal__close').on('click', function () {
+//    $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+//});
+
+//$('.button_mini').each(function (i) {
+//    $(this).on('click', function () {
+//        $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+//        $('.overlay, #order').fadeIn('slow');
+//    });
+//});
 
 
 
