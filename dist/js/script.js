574f5d491fa31dbf03831ebbd2018463a297a03f
azesm <azesm@yahoo.com>
2021-05-28 11:10:51 +0300

    end js

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,16 +1,37 @@
 $(document).ready(function () {
 
     const hamburger = document.querySelector('.hamburger'),
+        overlay = document.querySelector('.menu__overlay'),
         menu = document.querySelector('.menu'),
         closeElem = document.querySelector('.menu__close');
+        
 
     hamburger.addEventListener('click', () => {
         menu.classList.add('active');
     });
 
+    overlay.addEventListener('click', () => {
+        menu.classList.remove('active');
+    });
+
     closeElem.addEventListener('click', () => {
         menu.classList.remove('active');
     });
+
+
+    window.addEventListener("keydown", (e) => {
+        if (e.key == 'Escape') {
+            menu.classList.remove('active');
+        }
+    });
+
+    window.addEventListener('click', (e) => {
+        if (e.target == overlay) {
+            menu.classList.remove('active');
+        }
+    });
+
+
 
     const counters = document.querySelectorAll('.skills__ratings-counter'),
         lines = document.querySelectorAll('.skills__ratings-line span');
